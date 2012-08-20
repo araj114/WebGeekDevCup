d642beff3743d29c6003b477e6f7230d2023fe43
CuttingedgeDev <cuttingedgedev114@gmail.com>
2012-08-20 12:20:00 +0800

    Fixes profile and header

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -2,7 +2,7 @@ class Profile < ActiveRecord::Base
 	belongs_to :user, :dependent => :destroy
   attr_accessible :fname, :gender, :lname
 
- 	validates :fname, :lname, :gender. :presence => true
+ 	validates :fname, :lname, :gender, :presence => true
  	validates :fname, :length => 6..30
  	validates :lname, :length => 2..30
 end
